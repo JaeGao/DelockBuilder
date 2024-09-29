1b00800755ebd0e35bf4843dd6331e10b795e823
JaeGao <92616764+JaeGao@users.noreply.github.com>
2024-09-28 22:38:29 -0400

    jk

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
-    /* ...Your other config rules */
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
