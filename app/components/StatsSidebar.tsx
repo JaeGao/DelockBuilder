e35f6c87ddada86ba65633d275a888aa63dee538
JaeGao <jg6523@nyu.edu>
2024-10-16 19:05:45 -0400

    smaller stats bar

--- app/components/StatsSidebar.tsx
+++ app/components/StatsSidebar.tsx
@@ -215,7 +215,7 @@ const StatsSidebar: React.FC<StatsSidebarProps> = ({ characterStats, characterNa
     };
 
     return (
-        <div className="fixed top-0 right-0 w-1/5 min-w-[200px] h-screen bg-gray-900 overflow-y-auto transition-all duration-300 ease-in-out">
+        <div className="fixed top-0 right-0 w-1/6 min-w-[200px] h-screen bg-gray-900 overflow-y-auto transition-all duration-300 ease-in-out">
             <div className="sticky top-0 p-3 bg-gray-900 z-10 pb-2 mb-2 border-b border-gray-700">
                 <div className="flex mb-2">
                     <button
