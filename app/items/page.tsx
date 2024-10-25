1864e0cfdf176ab70daaf0c0f52314f06afb8fc0
JaeGao <jiaqi7277@gmail.com>
2024-10-25 19:23:27 -0400

    Update page.tsx

--- app/items/page.tsx
+++ app/items/page.tsx
@@ -10,6 +10,9 @@ export default async function ItemsPage() {
         <div>
             <Navbar />
             <div className="container mx-auto 2xl:ml-52 p-4">
+                <h1>this is scuffed but erm:
+                    “The featured typeface is Obviously from @ohnotypeco. I love it more than my own family.”
+                </h1>
                 <h1 className="text-3xl font-bold mb-6">Items</h1>
                 <ClientItemsWrapper initialItems={items} />
             </div>
