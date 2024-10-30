640cdb7fdd33f248e1316c6a44c78c0948f5741c
Picucu <33139604+Picucu@users.noreply.github.com>
2024-10-30 15:39:00 -0700

    Update CharacterBuilder.tsx
    fixed open div

--- app/components/CharacterBuilder.tsx
+++ app/components/CharacterBuilder.tsx
@@ -496,7 +496,7 @@ const CharacterBuilder: React.FC<CharacterBuilderProps> = ({ characterNameFromMa
                     />
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
